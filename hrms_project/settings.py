761e54dad3707056eebe76032b125e66d7f413ff
Rahul TR <trrahul33@gmail.com>
2025-10-24 10:23:02 +0530

    Timesheet approval condition change

--- hrms_project/settings.py
+++ hrms_project/settings.py
@@ -35,8 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 #
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
